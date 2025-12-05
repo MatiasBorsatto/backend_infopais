8211bab1cbf89d7a80b81b71eba0fe9dbeb086ba
MatiasBorsatto <matiasborsatto1@gmail.com>
2025-12-05 17:39:55 -0300

    Permiso de CORS a front del servidor

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors(
+    { origin: "http://localhost:5173", credentials: true },
+    { origin: "http://localhost:4000", credentials: true }
+  )
+);
 
 app.use(cookieParser());
 
